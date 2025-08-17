2ede2069889f6d1e39f83faccd5c8e2a2328d4b7
Your Name <youremail@example.com>
2025-08-16 17:29:20 -0700

    Refactor: Add error handling to CharacterRepository
    This commit updates the `CharacterRepository` to include error handling for network requests.
    
    -   **`CharacterRepository.kt`**:
        -   The `getCharacters` suspend function now includes a try-catch block to handle potential exceptions during the API call.
        -   If an exception occurs, it throws a new exception with a more descriptive message, including the original error message.

--- app/src/main/java/com/android/rickandmortymvvm/data/repository/CharacterRepository.kt
+++ app/src/main/java/com/android/rickandmortymvvm/data/repository/CharacterRepository.kt
@@ -5,8 +5,12 @@ import com.android.rickandmortymvvm.data.network.ApiService
 import com.android.rickandmortymvvm.data.network.RickandMortyApi
 
 class CharacterRepository(private val api: RickandMortyApi = ApiService.api) {
-
     suspend fun getCharacters(): List<Character> {
-        return api.getCharacters().results
+        return try {
+            api.getCharacters().results
+        }
+        catch (e: Exception) {
+            throw Exception("Failed to fetch characters ${e.message}")
+        }
     }
 }
